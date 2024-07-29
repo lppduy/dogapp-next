93e5744cf2f59a85e95d71a6489be84a441da4b4
lppduy <lephamphuongduy@gmail.com>
2024-07-29 16:49:39 +0700

    setup: init codebase with figma design link in README.md

---
+++ app/APP/COMPONENTS/AppMenuItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { FaDog } from "react-icons/fa";
+export default function AppMenuItem() {
+  return <div className="bg-red-500 w-[60px] h-[30px] CENTER">
+    <FaDog />
+  </div>;
+}
